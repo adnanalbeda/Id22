e340385dce7cc496b5a8093be17689335203023f
Adnan <adnan.albeda@outlook.com>
2024-09-13 15:58:38 +0300

    Explain why Guid.ToString()

--- Id22.EntityFrameworkCore/Id22Converter.cs
+++ Id22.EntityFrameworkCore/Id22Converter.cs
@@ -13,6 +13,11 @@ public class Id22Converters
             v => v.HasValue ? Id22.FromValue(v.Value) : null
         );
 
+    // Why using Guid.ToString() instead of Id22.ToString()
+    // 1- If id is generated in the db, it will be generated as full hexadecimal guid string.
+    // 2- Some ids can be conflicting if stored as ShortId.
+    // Like: jFIRNoMM-Ahy7FNXvdO6Ac and jFIRNoMM-Ahy7FNXvdO6AQ
+    // Both are resolved to the same Guid value: 8c521136-830c-f808-72ec-5357bdd3ba01
     public sealed class ValueToStringConverter()
         : ValueConverter<Id22, string?>(v => v.Value.ToString(), v => Id22.Parse(v));
 
