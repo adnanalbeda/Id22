3550d35ad2be5385442c79e32aeb29b014f7a8a5
Adnan <adnan.albeda@outlook.com>
2024-09-14 11:09:09 +0300

    Added Util: SafeParse

--- Id22/Parsers.cs
+++ Id22/Parsers.cs
@@ -8,11 +8,31 @@ public partial record struct Id22
     /// Tries to parse <paramref name="value"/> whether it's short id or regular guid value.
     /// </summary>
     /// <param name="value"></param>
-    /// <returns><see cref="Id22"/> of the value if parsing is successful. Otherwise, a default value.</returns>
-    public static Id22 Parse(string? s) =>
-        string.IsNullOrEmpty(s)
-            ? default
-            : FromValue(StringMatchesShortIdFormat(s) ? _GuidFromShortId(s) : Guid.Parse(s));
+    /// <returns><see cref="Id22"/> of the value if parsing is successful.</returns>
+    /// <exception cref="FormatException"/>
+    public static Id22 Parse(string? s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return default;
+
+        return FromValue(StringMatchesShortIdFormat(s) ? _GuidFromShortId(s) : Guid.Parse(s));
+    }
+
+    /// <summary>
+    /// Tries to parse
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static Id22 SafeParse(string? s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+            return default;
+
+        if (StringMatchesShortIdFormat(s))
+            return FromValue(_GuidFromShortId(s));
+
+        return Guid.TryParse(s, out Guid gid) ? FromValue(gid) : default;
+    }
 
     public static bool TryParse(
         [NotNullWhen(true)] string? s,
